a9762b22e79ce63ef30b5776b3cc32ea7e139413
yushi_jack <ihsuyadihcu@gmail.com>
2018-10-20 14:45:03 +0900

    写真の取り直し、シーン遷移の実装

---
+++ Smart Menu Client/Assets/Scripts/CheckPanel.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CheckPanel : MonoBehaviour {
+
+    [SerializeField]
+    MenuImage muneImage;
+    [SerializeField]
+    GameObject mainPanel;
+
+    public void OK(){
+        mainPanel.SetActive(true);
+        muneImage.MoveDown();
+        gameObject.SetActive(false);
+    }
+
+    public void Cancel()
+    {
+        SceneManager.LoadScene("Camera");
+    }
+}
--- Smart Menu Client/Assets/Scripts/MenuImage.cs
+++ Smart Menu Client/Assets/Scripts/MenuImage.cs
@@ -5,20 +5,24 @@ using UnityEngine.SceneManagement;
 public class MenuImage : MonoBehaviour {
 
     const int canvasWidthScale = 900;
+    const int menuImageMove = -150;
 
     [SerializeField]
     private RawImage rawImage;
 
-	// Use this for initialization
-	void Start () {
+    float scale;
+
+    // Use this for initialization
+    void Start () {
         //表示
         rawImage.texture = SMCamera.captureTexture;
         rawImage.SetNativeSize();
-        float scale = canvasWidthScale / rawImage.rectTransform.sizeDelta.x;
+        scale = canvasWidthScale / rawImage.rectTransform.sizeDelta.x;
         rawImage.rectTransform.localScale = Vector3.one * scale;
     }
 
-    public void OnClick(){
-        SceneManager.LoadScene("Main");
+    public void MoveDown(){
+        RectTransform rect = GetComponent<RectTransform>();
+        rect.anchoredPosition = new Vector2(rect.anchoredPosition.x, menuImageMove);
     }
 }
